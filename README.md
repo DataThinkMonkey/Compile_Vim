aea40e5b7c4b9c8d00c535b3487b8ae77be98800
jared bernard <jared@office.local>
2019-08-02 20:59:38 -0500

    Cleaned up script.

--- README.md
+++ README.md
@@ -3,18 +3,11 @@ Script to compile Vim 8.0 with python2 support and install Geeknote on Fedora 26
 
 The script does the following:
 
-1. Checks for git install.
-2. Installs dependencies for Vim 8.0.
+1. Removes all installations of vim.
+2. Installs dependencies for Vim 8.1.
 3. Clones Vim 8.0 repo.
-4. Configures, make and make installs.
-5. Clones my personal .vimrc file and .vim directory.
-6. Install dependencies for Geeknotes. 
-7. Installs python modules.
-8. Clone Geeknote.
-9. Installed Geeknote.
+4. Configures, make and make installs with full options. 
 
 **To-do**
+Clones my personal .vimrc file and .vim directory.
 
-1. Error checking and resolution.
-2. Include other installs and configures for a fresh Fedora install.
-   (Virtualbox, docker, insync, chrome, etc)
---
+++ compile_vim.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+# Description:
+#   Compile a full-featured Vim from source on Ubuntu/Debian distros. Based
+#   entirely on
+#   https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source
+#
+# Use:
+#   ./compile_full_vim.sh
+
+main(){
+	echo "y" | sudo apt-get remove \
+		vim \
+		vim-runtime \
+		gvim \
+		vim-tiny \
+		vim-common \
+		vim-gui-common
+	echo "y" | sudo apt-get install \
+		libncurses5-dev \
+		libgnome2-dev \
+		libgnomeui-dev \
+		libgtk2.0-dev \
+		libatk1.0-dev \
+		libbonoboui2-dev \
+		libcairo2-dev \
+		libx11-dev \
+		libxpm-dev \
+		libxt-dev \
+		python-dev ruby-dev \
+		mercurial
+
+	cd ~
+	git clone git@github.com:vim/vim.git --depth=1
+	cd vim
+	./configure --with-features=huge \
+		--enable-multibyte \
+		--enable-rubyinterp \
+		--enable-pythoninterp \
+		--with-python-config-dir=/usr/lib/python2.7/config \
+		--enable-perlinterp \
+		--enable-luainterp \
+		--enable-gui=gtk2 \
+		--enable-cscope \
+		--prefix=/usr
+	make VIMRUNTIMEDIR=/usr/share/vim/vim81
+	sudo make install
+    rm -Rf ~/vim
+}
+
+main
